94637da61e8b3ab8dcb63189bea7c78e9ac59e5c
SitiRamandhaAziz <39080786+SitiRamandhaAziz@users.noreply.github.com>
2019-04-16 09:33:49 +0700

    Update Neraca_model.php

--- Neraca_model.php
+++ Neraca_model.php
@@ -1,39 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-/*Class Neraca_model extends CI_Model
-{
-	function neraca_list(){
-		$hasil=$this->db->get('tes_form');
-		return $hasil->result();	
-	}
-	function save_neraca(){
-		$data=array(
-				'kassetarakas' => $this->input->post('kas_dan_setara_kas'),
-				'piutangusaha' => $this->input->post('piutang_usaha'),
 
-			);
-		$result=$this->db->insert('tes_form',$data);
-		return $result;
-	}
-
-	function update_neraca(){
-		$kas_dan_setara_kas=$this->input->post('kassetarakas');
-		$piutang_usaha=$this->input->post('piutangusaha');
-		$id=$this->input->post('id');
-
-		$this->db->set('neracaid',$id);
-		$this->db->set('kassetarakas',$kas_dan_setara_kas);
-		$this->db->set('piutangusaha',$piutang_usaha);
-		$result=$this->db->update('tes_form');
-		return $result;
-	}
-	function delete_neraca(){
-		$id=$this->input->post('neracaid');
-		$this->db->where('id',$id);
-		$result=$this->db->delete('tes_form');
-		return $result;
-	}
-}*/
 
 class Neraca_model extends CI_Model{
 
